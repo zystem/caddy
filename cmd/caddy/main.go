c7218669b4687e6e8fe479bd3c092c857dc09f24
andrii.zagriadskyi <azystem@gmail.com>
2025-04-30 17:03:05 +0300

    patched webdav added

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -34,6 +34,7 @@ import (
 	// plug in Caddy modules here
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/zystem/caddy-webdav"
+	webdav "github.com/zystem/caddy-webdav"
 )
 
 func main() {
